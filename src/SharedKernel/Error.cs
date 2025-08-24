faca11b232666636c945419b0c8b11af44da1617
Patryk Kowalczyk <papyszoo@gmail.com>
2025-08-25 00:31:56 +0200

    removed initial project, started working on adding clean architecture

---
+++ src/SharedKernel/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel
+{
+    public sealed record Error(string Code, string Message)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+        public static readonly Error NullValue = new("Error.NullValue", "Null value was provided");
+
+        public static implicit operator Result(Error error) => Result.Failure(error);
+    }
+}
