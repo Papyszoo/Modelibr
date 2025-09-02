916f017bfa87bbcfa00b86263fbec538f1a4884d
Patryk Kowalczyk <papyszoo@gmail.com>
2025-09-02 11:01:28 +0200

    Added clean architecture projects, docker compose file and started working on mssql database

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Abstractions.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Application
 {
@@ -6,8 +8,31 @@ namespace Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediator();
+            return services;
+        }
+
+        private static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            services.RegisterHandlersForInterfaceTypes(assembly, [typeof(IQueryHandler<,>), typeof(ICommandHandler<>), typeof(ICommandHandler<,>)]);
 
             return services;
+        }
+
+        private static void RegisterHandlersForInterfaceTypes(this IServiceCollection services, Assembly assembly, IEnumerable<Type> interfaceTypes)
+        {
+            var handlerTypes = assembly.GetTypes()
+                .Where(t => !t.IsAbstract && !t.IsInterface)
+                .SelectMany(t => t.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                .Where(ti => ti.Interface.IsGenericType && interfaceTypes.Any(it => it == ti.Interface.GetGenericTypeDefinition()))
+                .Select(ti => new { ImplementationType = ti.Type, ServiceType = ti.Interface });
+
+            foreach (var handler in handlerTypes)
+            {
+                services.AddScoped(handler.ServiceType, handler.ImplementationType);
+            }
         }
     }
 }
