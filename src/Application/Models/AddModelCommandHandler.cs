78db59e140c0b13c719017c2ce7e358823cbcda8
Patryk Kowalczyk <papyszoo@gmail.com>
2025-09-03 00:14:28 +0200

    adjusted docker-compose, started working on ef configuration, started working on first command

---
+++ src/Application/Models/AddModelCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Models
+{
+    internal class AddModelCommandHandler : ICommandHandler<AddModelCommand, AddModelCommandResponse>
+    {
+        public Task<Result<AddModelCommandResponse>> Handle(AddModelCommand command, CancellationToken cancellationToken)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
