34e96cfa8e52f184d4d628d0ad539de2eeadfc37
potato\praca <papyszoo@gmail.com>
2025-09-02 11:01:28 +0200

    Added clean architecture projects, docker compose file and started working on mssql database

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
+            {
+                optionsBuilder.UseSqlServer();
+            });
+
+            return services;
+        }
+    }
+}
